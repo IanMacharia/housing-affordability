0f6fa0de26cfba3532bc4e76687c4a7356912f48
Mwangi <ianmwangim@outlook.com>
2025-10-12 15:46:03 -0400

    chore: normalize EOF/EOL with pre-commit

--- src/App.py
+++ src/App.py
@@ -1,6 +1,7 @@
-﻿import os
-from dotenv import load_dotenv
+import os
+
 import streamlit as st
+from dotenv import load_dotenv
 
 load_dotenv()
 st.set_page_config(page_title="Housing Affordability Dashboard", layout="wide")
